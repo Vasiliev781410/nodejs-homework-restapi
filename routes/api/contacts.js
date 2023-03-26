f00b270d080efa108ecca54cb7777078dd79cc35
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2023-03-26 18:51:37 +0300

    50%

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,12 +4,26 @@ const contacts = require("../../models/contacts")
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result)
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);    
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  } 
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message test' })
+  try {
+    const {contactId} = req.params;  
+    const result = await contacts.getContactById(contactId);
+    if (!result){          
+      return res.status(404).json({message: `Book with id ${contactId} not found`});
+    }
+    
+    res.json(result);    
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  } 
 })
 
 router.post('/', async (req, res, next) => {
