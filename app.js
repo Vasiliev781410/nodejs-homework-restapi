57a3d481478c3ae1f3f11c78b7ab7dd575e69236
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2023-04-24 16:02:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.static("public"));
 
 app.use('/api/users', authRouter);
 app.use('/api/contacts', contactsRouter);
+app.use('/api/business-processes', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
